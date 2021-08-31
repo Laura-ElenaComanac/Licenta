f4ee88a873a67c72fb0ddf0e2b1d53f96e1f103e
Laura-Elena Olaru <laura-elena.olaru@192-168-0-118.rdsnet.ro>
2021-08-31 15:23:59 +0300

    The beginning

---
+++ ProjectSocialNetwork/src/main/java/com/license/ProjectSocialNetwork/ProjectSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.license.ProjectSocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSocialNetworkApplication.class, args);
+	}
+
+}
